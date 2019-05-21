327a4924b51b64a586f40a84b1f61317b8336695
Carlos Rodriguez <cs.rodi@gmail.com>
2019-05-21 21:06:42 +0200

    Add sounds and play on tap action

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MainApp());
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AudioCache audioCache = AudioCache();
+
+    return MaterialApp(
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.grey[200],
         appBarTheme: AppBarTheme(color: Colors.black),
@@ -12,12 +19,19 @@ void main() {
         appBar: AppBar(
           title: Text('I Am Your Father'),
         ),
-        body: Center(
-          child: Image(
-            image: AssetImage('images/vader.png'),
+        body: Ink.image(
+          image: AssetImage('assets/images/vader.png'),
+          child: InkWell(
+            onTap: () {
+              playSound(audioCache);
+            },
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
+
+  playSound(AudioCache audioCache) async {
+    audioCache.play('sounds/father.mp3', mode: PlayerMode.LOW_LATENCY);
+  }
 }
